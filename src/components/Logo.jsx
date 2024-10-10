ffed5b1ca560e60e551b0c5cfd57b4ce22fcb021
Twinkle Shaw <twinkleshaw68216@gmail.com>
2024-10-10 20:19:01 +0530

    added changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 function Logo() {
   return (
-    <div className="text-2xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-transparent bg-clip-text font-bold">
+    <div className="text-2xl bg-gradient-to-r from-white via-orange-300 to-yellow-200 text-transparent bg-clip-text font-bold">
       <i>Blogosphere</i>
     </div>
   );
